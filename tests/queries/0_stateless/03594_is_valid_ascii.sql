e16df63d0c09ee924b51ef243e7793bf154152ac
hm-5el <226357329+hm-5el@users.noreply.github.com>
2025-08-14 20:25:41 +0200

    make test queries consistent.

--- tests/queries/0_stateless/03594_is_valid_ascii.sql
+++ tests/queries/0_stateless/03594_is_valid_ascii.sql
@@ -1,38 +1,38 @@
 -- constant input
-select 1 = isValidASCII('');
-select 1 = isValidASCII('some text');
-select 0 = isValidASCII('какой-то текст');
-select 1 = isValidASCII('\x00');
-select 1 = isValidASCII('\x7F');
-select 0 = isValidASCII('\x80');
-select 1 = isValidASCII('\x00\x7F');
-select 1 = isValidASCII('\x7F\x00');
-select 0 = isValidASCII('\xC2\x80');
-select 0 = isValidASCII('\x7F\x80');
-select 0 = isValidASCII('\x70\x70\x80');
-select 0 = isValidUTF8('\x66\x80\x00');
-select 1 = isValidASCII(repeat('\x7F\x00', 200));
-select 0 = isValidASCII(repeat('\x7F\x80', 200));
+SELECT 1 = isValidASCII('');
+SELECT 1 = isValidASCII('some text');
+SELECT 0 = isValidASCII('какой-то текст');
+SELECT 1 = isValidASCII('\x00');
+SELECT 1 = isValidASCII('\x7F');
+SELECT 0 = isValidASCII('\x80');
+SELECT 1 = isValidASCII('\x00\x7F');
+SELECT 1 = isValidASCII('\x7F\x00');
+SELECT 0 = isValidASCII('\xC2\x80');
+SELECT 0 = isValidASCII('\x7F\x80');
+SELECT 0 = isValidASCII('\x70\x70\x80');
+SELECT 0 = isValidUTF8('\x66\x80\x00');
+SELECT 1 = isValidASCII(repeat('\x7F\x00', 200));
+SELECT 0 = isValidASCII(repeat('\x7F\x80', 200));
 
 -- fixed size constant input
-select 1 = isValidASCII(toFixedString('', 1));
-select 1 = isValidASCII(toFixedString('some text', 9));
-select 0 = isValidASCII(toFixedString('какой-то текст', 26));
-select 1 = isValidASCII(toFixedString('\x00', 1));
-select 1 = isValidASCII(toFixedString('\x7F', 1));
-select 0 = isValidASCII(toFixedString('\x80', 1));
-select 1 = isValidASCII(toFixedString('\x00\x7F', 2));
-select 1 = isValidASCII(toFixedString('\x7F\x00', 2));
-select 0 = isValidASCII(toFixedString('\xC2\x80', 2));
-select 0 = isValidASCII(toFixedString('\x7F\x80', 2));
-select 0 = isValidASCII(toFixedString('\x70\x70\x80', 3));
-select 0 = isValidUTF8(toFixedString('\x66\x80\x00', 3));
-select 1 = isValidASCII(toFixedString(repeat('\x7F\x00', 200), 400));
-select 0 = isValidASCII(toFixedString(repeat('\x7F\x80', 200), 400));
+SELECT 1 = isValidASCII(toFixedString('', 1));
+SELECT 1 = isValidASCII(toFixedString('some text', 9));
+SELECT 0 = isValidASCII(toFixedString('какой-то текст', 26));
+SELECT 1 = isValidASCII(toFixedString('\x00', 1));
+SELECT 1 = isValidASCII(toFixedString('\x7F', 1));
+SELECT 0 = isValidASCII(toFixedString('\x80', 1));
+SELECT 1 = isValidASCII(toFixedString('\x00\x7F', 2));
+SELECT 1 = isValidASCII(toFixedString('\x7F\x00', 2));
+SELECT 0 = isValidASCII(toFixedString('\xC2\x80', 2));
+SELECT 0 = isValidASCII(toFixedString('\x7F\x80', 2));
+SELECT 0 = isValidASCII(toFixedString('\x70\x70\x80', 3));
+SELECT 0 = isValidUTF8(toFixedString('\x66\x80\x00', 3));
+SELECT 1 = isValidASCII(toFixedString(repeat('\x7F\x00', 200), 400));
+SELECT 0 = isValidASCII(toFixedString(repeat('\x7F\x80', 200), 400));
 
 -- alias
-select 1 = isASCII('some text');
-select 1 = isASCII(toFixedString('some text', 9));
+SELECT 1 = isASCII('some text');
+SELECT 1 = isASCII(toFixedString('some text', 9));
 
 -- column input
 DROP TABLE IF EXISTS asciis;
@@ -57,7 +57,7 @@ SELECT isValidASCII(val) FROM asciis ORDER BY val;
 DROP TABLE asciis;
 
 -- unsupported arguments
-select isValidUTF8(['\x00', '\x7F']); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+SELECT isValidUTF8(['\x00', '\x7F']); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 SELECT isValidUTF8(toUUID('00000000-0000-0000-0000-000000000000')); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
 SELECT isValidUTF8(toIPv6('127.0.0.1')); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
-select isValidUTF8(toIPv4('127.0.0.1')); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
+SELECT isValidUTF8(toIPv4('127.0.0.1')); -- { serverError ILLEGAL_TYPE_OF_ARGUMENT }
